4c49adedc23e9eff933273a298083d4b6b708848
Toby <609987101@qq.com>
2020-11-12 14:40:46 +0800

    Add premake5.lua for docking branch

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_internal.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_widgets.cpp",
+        "imgui.cpp",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+		}
+
+		defines 
+		{ 
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
